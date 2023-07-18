8a31ef615a04dcb181e73be67aee83bdead1ba22
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-18 14:29:03 +0200

    second version

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ const color = prompt('inserisci il tuo colore preferito');
 
 const password = (userName + userSurname + color) +'21';
 
-document.getElementById('password').innerHTML = password
+// document.getElementById('password').innerHTML = password; 
+
+// alert.getElementDbyId = (password);
 
 
-
+window.alert(password);
 
 
 
